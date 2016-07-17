3d85592efb835bed6c7f55e5e394a0e2e7699629
Aperçu <bgronon@gmail.com>
2016-07-17 16:40:23 +0200

    deep-freeze action instead of stateAfter
    Closes #2.

--- src/reducerTest.js
+++ src/reducerTest.js
@@ -3,7 +3,7 @@ import deepFreeze from 'deep-freeze'
 export default (reducer, stateBefore, action, stateAfter, description) => (
 	(t) => {
 		deepFreeze(stateBefore)
-		deepFreeze(stateAfter)
+		deepFreeze(action)
 
 		t.deepEqual(
 			reducer(stateBefore, action()),
